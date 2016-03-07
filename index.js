5a2862ff1db52b1ed57b0b0174f41f624940b313
Michael Bykov <m.bykov@gmail.com>
2016-03-07 20:07:59 +0300

    i

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // morpheus v.0.3.0
 
 var _ = require('underscore');
-var sandhi = require('sandhi');
-var c = sandhi.const;
-var u = sandhi.u;
-var s = sandhi.sandhi;
+var s = require('sandhi');
+var c = s.const;
+var u = s.u;
+var sandhi = s.sandhi;
+var outer = s.outer;
 var log = u.log;
 var p = u.p;
 var rasper = require('flakes');
 
-dbpath = 'http://localhost:5984';
+dbpath = 'http://admin:kjre4317@localhost:5984';
 var Relax = require('relax-component');
 var relax = new Relax(dbpath);
 relax.dbname('gita-add');
@@ -21,46 +22,86 @@ function morpheus() {
     this.queries = [];
 }
 
+/*
+  outer:
+  во-вторых, каждому flake на -o нужно сразу добавить flake на visarga, etc ?
+  а есть-ли вообще у меня в словаре слова с окончанием на o, на e? есть, пара слов и обращения
+  непонятно - просто добавить висаргу? что потом?
+  или - только в последнюю pada в цепочке?
+  выписать все реальные слова на -o в словаре? Пока список:
+  - अहो = alas - अथो = or in other words - नो = nor - आहो = or else - все они попали бы в список terms, если бы стояли отдельно
+  или просто outer(samasa), но кроме этого списка?
+  outer заменит -o, а в ответе исключение - aho
+  то есть outer, и затем, если в цепочке ahaH, добавить aho, etc на эти исключения
+  по честному, нужно учитывать next, но в тестах я gita-add next не сделал, для простоты
+  ==> нельзя вообще в тестах работать с gita-add, потому что там формы вырваны из контекста.
+  а в реале будет задан какой-то конкретный next
+*/
+
 // main
-morpheus.prototype.run = function(samasa, cb) {
-    var clean = correctM(samasa);
+// должен возвращать полностью сформированный список вариантов с весами-вероятностями
+morpheus.prototype.run = function(samasa, next, cb) {
+    if (!next) next = '';
+    // var clean = correctM(samasa);
+    var clean = outer(samasa, next);
+    // log('CLEAN', clean);
     var flakes = rasper.cut(clean);
     // p(flakes);
+    // log('FLAKES size:', flakes.length);
     var stems = _.uniq(_.flatten(flakes));
-    // log(stems);
+    // log('STEMS to get', stems.length);
     getDicts(stems, function(err, dbdicts) {
-        log('DBD', dbdicts);
+        // log('DBD', dbdicts);
         // TODO: теперь установить соответствие между flakes и dbdicts
-        // с учетом того, что в словаре Гиты dicts повторяются
+        cb(dbdicts);
     });
-    cb('ok');
+    // cb('ok');
     return;
 }
 
 
 // забрать реально существующие padas из BD
 function getDicts(stems, cb) {
-    // stems = ["क्तात्मा","तात्मा","आत्मा","अत्मा","त्मा","मा"];
-    var keys = ['keys=', JSON.stringify(stems)].join('');
-    // var qkeys = 'keys=' + JSON.stringify(stems);
+    var keys = {keys: stems};
     var view = 'gita-add/byForm';
-    // var view = 'sa/stem';
-    log('SSS-new', keys);
-    // qkeys = keys=["क्तात्मा","तात्मा","आत्मा","अत्मा","त्मा","मा"];
-    // qkeys = 'keys=["क्तात्मा","तात्मा","आत्मा","अत्मा","त्मा","मा"]';
-    // log('SSS-new', qkeys);
+    // log('morph-03 getDicts - SSS-new', keys.length);
+    // FIXME: некузяво, keys вручную отдельно посылается через .send
+    // и Content-Type отдельно прописан - так нельзя
+    relax
+        .postView(view)
+        .send(keys)
+        // .query({limit: 100})
+        // .query({include_docs: true})
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+            if (err) log('ERR morph getDicts', err, res);
+            if (err) return cb(err, null);
+            var rows = JSON.parse(res.text.trim()).rows;
+            var docs = _.map(rows, function(row) {
+                var doc = {stem: row.key, trn: row.value};
+                return doc;
+            });
+            cb(err, docs);
+        });
+}
+
+function getDicts_(stems, cb) {
+    var keys = ['keys=', JSON.stringify(stems)].join('');
+    var view = 'gita-add/byForm';
+    log('morph-03 getDicts - SSS-new', keys.length);
     relax
         .view(view)
         .query(keys)
-        // .query({include_docs: true})
+    // .query({include_docs: true})
         .query({limit: 100})
         .end(function(err, res) {
+            if (err) log('ERR morph getDicts', err, res);
             if (err) return cb(err, null);
             var rows = JSON.parse(res.text.trim()).rows;
             // log('ROWS', rows);
             // var docs = _.map(rows, function(row) {
-                // var doc = row.doc;
-                // return doc;
+            // var doc = row.doc;
+            // return doc;
             // });
             // в Гите значения dicts повторяются с разными переводами, в других словарях нужен будет .doc
             var docs = _.map(rows, function(row) {
--- test/gita.js
+++ test/gita.js
@@ -17,7 +17,7 @@ var inc = u.include;
 
 var Relax = require('relax-component');
 var relax = new Relax('http://admin:kjre4317@localhost:5984');
-relax.dbname('gita-add');
+relax.dbname('gita');
 
 var morph = require('../index');
 
@@ -25,21 +25,81 @@ runGitaTests();
 
 function runGitaTests() {
     getDocs(function(docs) {
-        docs = docs.slice(-20);
+        docs = docs.slice(3,7);
+        // var ok;
+        var form, next, nextLine, samasa, trn, pdch;
+        var dicts;
         docs.forEach(function(doc, idx) {
-            //
+            // log('D', idx, doc);
+            doc.lines.forEach(function(line, idy) {
+                if (line.trn) return;
+                form = line.form;
+                nextLine = doc.lines[idy+1];
+                next = (nextLine) ? nextLine.form : null;
+                if (next == '।') next = null;
+                if (form == '।') return;
+                samasa = outer(form, next);
+                if (form != samasa) log('L', idy, form, 'next:', next, 'clean', samasa);
+                pdch = line.dicts.map(function(dict) { return dict.form});
+
+                var salat = salita.sa2slp(samasa)
+                // log('PDCH', salat, samasa, 'pdch:', pdch);
+                // morph забирает dicts из gita-add
+                morph.run(samasa, next, function(dicts) {
+                    log('idx', idx, salat, samasa);
+                    if (!dicts || dicts.length == 0) {
+                        log('NO DICTS idx:', idx, 'salat', salat, 'samasa', samasa);
+                    }
+                    var stems = dicts.map(function(dict) { return dict.stem});
+                    log('PDCH', salat, samasa, 'pdch:', pdch, 'stems:', stems);
+                    pdch.forEach(function(pada) {
+                        if (inc(stems, pada)) ok = true;
+                        else ok = false;
+                    });
+                    if (!ok) {
+                        log('ABSENT', line);
+                        throw new Error();
+                    } else {
+                        log('OK')
+                    }
+                }); // morph
+
+            });
+        //     if (doc.trns) return;
+        //     var samasa = doc.form;
+        //     var nextDoc = docs[idx+1];
+        //     var next = (nextDoc) ? nextDoc.form : '';
+        //     log('SAMASA', samasa, 'NEXT', next);
+        //     if (samasa.length > 19) return;
+        //     var pdchs = doc.pdchs.map(function(pdch) { return pdch.split('-')});
         });
-        log('DOCS', docs);
+        // log('DOCS', docs);
     });
 }
 
 
+// нельзя забирать через .all, потому что next не тот.
 function getDocs(cb) {
+    relax
+        .all([])
+    // .query(query)
+        .query({include_docs: true})
+        // .query({limit: 10000})
+        .end(function(err, res) {
+            if (err) cb(err);
+            // log('DDD', res.text.trim())
+            var rows = JSON.parse(res.text.trim()).rows;
+            var docs = _.map(rows, function(row) { return row.doc });
+            cb(docs);
+        });
+}
+
+function getDocs_(cb) {
     var view = 'gita-add/byPdch';
     relax
         .view(view)
     // .query(query)
-        // .query({include_docs: true})
+    // .query({include_docs: true})
         .query({limit: 10000})
         .end(function(err, res) {
             if (err) cb(err);
